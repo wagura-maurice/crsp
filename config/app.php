062cdca3fce44449a2488739b741c406e5c6d2bd
Wagura Maurice <business@waguramaurice.com>
2024-10-13 15:05:57 +0300

    update app.php

--- config/app.php
+++ config/app.php
@@ -123,4 +123,7 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    // Define the areas
+    'areas' => ['Westlands', 'Starehe', 'Mathare', 'Makadara', 'Langata', 'Kibra', 'Kasarani', 'Njiru', 'Kamukunji', 'Dagoretti', 'Embakasi']
+
 ];
